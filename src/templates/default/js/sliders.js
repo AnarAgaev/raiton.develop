cd500068bfeef8ba7b6ec0a6ccd99a1ab0cf9440
Anar Agaev <anar.n.agaev@gmail.com>
2022-01-17 17:18:31 +0300

    Added slider and consultation text

--- src/templates/default/js/sliders.js
+++ src/templates/default/js/sliders.js
@@ -1,18 +1,18 @@
 $(document).ready(() => {
-    // // Слайдер с результатами
-    // window.resultsSlider = new Swiper('.swiper', {
-    //     slidesPerView: 1,
-    //     speed: 300,
-    //     preloadImages: true,
-    //     observer: true,
-    //     observeParents: true,
-    //     observeSlideChildren: true,
-    //     autoHeight: false,
-    //     spaceBetween: 15,
-    //     pagination: {
-    //         el: '.swiper-pagination',
-    //         clickable: true,
-    //         dynamicBullets: true,
-    //     }
-    // });
+    // Слайдер с результатами
+    window.resultsSlider = new Swiper('.swiper', {
+        slidesPerView: 1,
+        speed: 300,
+        preloadImages: true,
+        observer: true,
+        observeParents: true,
+        observeSlideChildren: true,
+        autoHeight: false,
+        spaceBetween: 15,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            dynamicBullets: true,
+        }
+    });
 });
