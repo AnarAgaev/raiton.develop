492410ebc369c7dd2590a09988fd1cc6da4d21b2
Anar Agaev <anar.n.agaev@gmail.com>
2022-01-18 12:43:25 +0300

    Added sale section and feedback slider

--- src/templates/default/js/sliders.js
+++ src/templates/default/js/sliders.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
     // Слайдер с результатами
-    window.resultsSlider = new Swiper('.swiper', {
+    window.resultsSlider = new Swiper('.swiper_results', {
         slidesPerView: 1,
         speed: 300,
         preloadImages: true,
@@ -8,11 +8,37 @@ $(document).ready(() => {
         observeParents: true,
         observeSlideChildren: true,
         autoHeight: false,
-        spaceBetween: 15,
+        //spaceBetween: 15,
         pagination: {
-            el: '.swiper-pagination',
+            el: '.swiper-pagination-results-items',
             clickable: true,
             dynamicBullets: true,
         }
     });
+
+    // Слайдер с отзыывами
+    new Swiper('.swiper_feedback', {
+        slidesPerView: 1,
+        speed: 300,
+        preloadImages: true,
+        observer: true,
+        observeParents: true,
+        observeSlideChildren: true,
+        autoHeight: false,
+        pagination: {
+            el: '.swiper-pagination-feedback',
+            clickable: true,
+            dynamicBullets: true,
+        },
+        breakpoints: {
+            768: {
+                centeredSlides: true,
+                initialSlide: 1,
+                slidesPerView: 3,
+            }
+        }
+    });
+
+
+
 });
