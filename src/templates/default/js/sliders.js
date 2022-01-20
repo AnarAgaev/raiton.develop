56b35f798772724de1ceb19658e0b2ba0977249b
Anar Agaev <anar.n.agaev@gmail.com>
2022-01-20 16:52:37 +0300

    Adaptived results slider

--- src/templates/default/js/sliders.js
+++ src/templates/default/js/sliders.js
@@ -1,20 +1,87 @@
 $(document).ready(() => {
     // Слайдер с результатами
-    window.resultsSlider = new Swiper('.swiper_results', {
-        slidesPerView: 1,
-        speed: 300,
-        preloadImages: true,
-        observer: true,
-        observeParents: true,
-        observeSlideChildren: true,
-        autoHeight: false,
-        //spaceBetween: 15,
-        pagination: {
-            el: '.swiper-pagination-results-items',
-            clickable: true,
-            dynamicBullets: true,
-        }
-    });
+    window.resultsSlider = undefined;
+
+    const initResultSlider = () => {
+        window.resultsSlider = new Swiper('.swiper_results', {
+            slidesPerView: 1,
+            speed: 300,
+            preloadImages: true,
+            observer: true,
+            observeParents: true,
+            observeSlideChildren: true,
+            autoHeight: false,
+            pagination: {
+                el: '.swiper-pagination-results-items',
+                clickable: true,
+                dynamicBullets: true,
+            },
+            breakpoints: {
+                768: {
+                    spaceBetween: 20,
+                    slidesPerView: 2,
+                },
+                1140: {
+                    spaceBetween: 20,
+                    slidesPerView: 3,
+                }
+            }
+        });
+    };
+
+    initResultSlider();
+
+    // const destroyResultsSlider = () => {
+    //     if (resultsSlider !== undefined) {
+    //
+    //         resultsSlider.length
+    //             ? resultsSlider.forEach(slider => slider.destroy())
+    //             : resultsSlider.destroy();
+    //     }
+    // };
+
+    // $(window).resize(() => {
+    //    const windowWidth = $(window).width(),
+    //        breakPointXL = 1140,
+    //        screenVersion = document.body.dataset.screen,
+    //
+    //        currentScreenVersion = (windowWidth > breakPointXL)
+    //            ? 'desktop'
+    //            : 'mobile';
+    //
+    //    if (screenVersion !== currentScreenVersion) {
+    //        setScreenVersion();
+    //        checkResultsSlider();
+    //    }
+    // });
+
+    // const setScreenVersion = () => {
+    //     const windowWidth = $(window).width(),
+    //         breakPointXL = 1140;
+    //
+    //     document
+    //         .body
+    //         .dataset
+    //         .screen = (windowWidth > breakPointXL)
+    //         ? 'desktop'
+    //         : 'mobile';
+    // };
+
+    // // Устанавливаем значение для
+    // // ширины экрана при первой загрузке
+    // setScreenVersion();
+
+    // const checkResultsSlider = () => {
+    //     const windowWidth = $(window).width(),
+    //         breakPointXL = 1140;
+    //
+    //     windowWidth > breakPointXL
+    //         ? destroyResultsSlider()
+    //         : initResultSlider();
+    // };
+
+    // // Чекаем слайдер при первой загрузке
+    // checkResultsSlider();
 
     // Слайдер с отзыывами
     new Swiper('.swiper_feedback', {
